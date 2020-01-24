eee40338a676ba1771eb6c14f43ab7082609bbaf
MetalDanyboy <dsepulvedatrangol@gmail.com>
2020-01-24 20:33:04 -0300

    modified:   ProyectoIntro/__pycache__/settings.cpython-38.pyc 	modified:   db.sqlite3 	modified:   gestion/__pycache__/__init__.cpython-38.pyc 	modified:   gestion/__pycache__/admin.cpython-38.pyc 	modified:   gestion/__pycache__/models.cpython-38.pyc 	modified:   gestion/admin.py 	modified:   gestion/migrations/0001_initial.py 	deleted:    gestion/migrations/0002_auto_20200123_2149.py 	new file:   gestion/migrations/0002_auto_20200124_1944.py 	modified:   gestion/migrations/__pycache__/0001_initial.cpython-38.pyc 	deleted:    gestion/migrations/__pycache__/0002_auto_20200123_2149.cpython-38.pyc 	new file:   gestion/migrations/__pycache__/0002_auto_20200124_1944.cpython-38.pyc 	modified:   gestion/migrations/__pycache__/__init__.cpython-38.pyc 	modified:   gestion/models.py

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from gestion.models import *
-
 # Register your models here.
 
-admin.site.register(Profesores)
-admin.site.register(Estudiantes)
-#esto ya no esta
 
+class profesor_admin(admin.ModelAdmin):
+    list_display=("nombres","apellidos","email")
+    search_fields=("nombres","apellidos")
+
+
+class estudiante_admin(admin.ModelAdmin):
+    list_display=("nombres","apellidos","curso")
+    search_fields=("nombres","apellidos","curso")
+    list_filter=("curso",)
+
+class apoderado_admin(admin.ModelAdmin):
+    list_display=("nombres","apellidos","pupilo")
+    search_fields=("nombres","apellidos","pupilo")
+
+admin.site.register(Profesor, profesor_admin)
+admin.site.register(Estudiante, estudiante_admin)
+admin.site.register(Apoderado, apoderado_admin)
--- gestion/models.py
+++ gestion/models.py
@@ -2,18 +2,36 @@ from django.db import models
 
 # Create your models here.
 
-class Profesores(models.Model):
-    nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
+class Profesor(models.Model):
+    nombres=models.CharField(max_length=40)
+    apellidos=models.CharField(max_length=40)
     email=models.EmailField()
-    telefono=models.CharField(max_length=7)
+    direccion=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=11)
 
-class Estudiantes(models.Model):
-    nombre=models.CharField(max_length=30)
-    curso=models.CharField(max_length=20)
-    edad=models.IntegerField()
+    def __str__(self):
+        return "{} {}".format(self.nombres,self.apellidos)
 
-class Pedidos(models.Model):
-    numero=models.IntegerField()
-    fecha=models.DateField()
-    entregado=models.BooleanField()
+
+class Estudiante(models.Model):
+    nombres=models.CharField(max_length=40)
+    apellidos=models.CharField(max_length=40)
+    email=models.EmailField()
+    direccion=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=11)
+    curso=models.CharField(max_length=2)
+    apoderado=models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{} {}".format(self.nombres,self.apellidos)
+
+class Apoderado(models.Model):
+    nombres=models.CharField(max_length=40)
+    apellidos=models.CharField(max_length=40)
+    email=models.EmailField()
+    direccion=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=11)
+    pupilo=models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{} {}".format(self.nombres,self.apellidos)
