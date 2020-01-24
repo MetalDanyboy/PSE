ad4ea19b459afa747d4ad66cf18be0d7f6d053ac
MetalDanyboy <dsepulvedatrangol@gmail.com>
2020-01-24 17:19:44 -0300

    Agregando todo a github

---
+++ gestion/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from gestion.models import *
+
+# Register your models here.
+
+admin.site.register(Profesores)
+admin.site.register(Estudiantes)
+admin.site.register(Pedidos)
