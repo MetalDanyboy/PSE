e19b9bfa514c0c649931c9ad89351d1de27f9b37
PedroPapelillo <59874747+PedroPapelillo@users.noreply.github.com>
2020-03-20 23:53:30 -0300

    Login required
    Agregado a todas las páginas necesarias

--- ProyectoIntro/views.py
+++ ProyectoIntro/views.py
@@ -3,6 +3,7 @@ from django.template import Template, Context
 from django.template import loader
 from django.shortcuts import render
 from gestion.models import Estudiante, Cursos
+from django.contrib.auth.decorators import login_required
 
 def PSE_login(request):
 	return render(request, "PSE_login.html")
@@ -10,31 +11,39 @@ def PSE_login(request):
 def PSE_forgotpassword(request):
 	return render(request, "PSE_forgotpassword.html")
 
+@login_required
 def PSE_profesores(request):
     return render(request, "profesores/PSE_profesores.html")
 
+@login_required
 def PSE_profesores_curso_calificaciones(request):
 	return render(request, "profesores/PSE_profesores_curso_calificaciones.html")
 
+@login_required
 def PSE_profesores_cursos(request):
 	cursos=Cursos.objects.filter()
 	estudiantes=Estudiante.objects.filter()
 	return render(request, "profesores/PSE_profesores_cursos.html",{"estudiantes":estudiantes,"cursos":cursos})
 
+@login_required
 def PSE_profesores_perfil_profesor(request):
 	return render(request, "profesores/PSE_profesores_perfil_profesor.html")
 
+@login_required
 def PSE_profesores_alumno(request,user):
 	estudiante=Estudiante.objects.filter(id__icontains=user)
 	return render(request, "profesores/PSE_profesores_alumno.html",{"estudiante":estudiante})
 
+@login_required
 def PSE_profesores_alumno_info(request,user):
 	estudiante=Estudiante.objects.filter(id__icontains=user)
 	return render(request, "profesores/PSE_profesores_alumno_info.html",{"estudiante":estudiante})
 
+@login_required
 def PSE_profesores_alumno_notas(request):
 	return render(request, "profesores/PSE_profesores_alumno_notas.html")
 
+@login_required
 def PSE_profesores_alumno_progreso(request):
 	return render(request, "profesores/PSE_profesores_alumno_progreso.html")
 
@@ -44,5 +53,6 @@ def PSE_test(request):
 
 	return render(request, "profesores/test.html",{"estudiantes":estudiantes} )
 
+@login_required
 def PSE_obs_por_curso(request):
 	return render(request, "profesores/PSE_profesores_observaciones_por_curso.html")
